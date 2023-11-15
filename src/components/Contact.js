918bc5e0fdce3b825e4c269a7a73da1e73751467
Ralph Jason Burgos <91693827+rlphjyson@users.noreply.github.com>
2023-11-15 16:01:09 +0800

    my-portfolio

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,29 @@
-
 import React from "react";
 
 export default function Contact() {
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [message, setMessage] = React.useState("");
+
+    function encode(data) {
+        return Object.keys(data)
+            .map(
+                (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+            )
+            .join("&");
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", name, email, message }),
+        })
+            .then(() => alert("Message sent!"))
+            .catch((error) => alert(error));
+    }
+
     return (
         <section id="contact" className="relative">
             <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
@@ -15,8 +37,7 @@ export default function Contact() {
                         marginHeight={0}
                         marginWidth={0}
                         style={{ filter: "opacity(0.7)" }}
-
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61916.16484610675!2d121.06766144371429!3d14.091322201837656!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd6f72ec612957%3A0xb38df4072fe465d6!2sTanauan%2C%20Batangas!5e0!3m2!1sen!2sph!4v1700032657824!5m2!1sen!2sph"
+                        src="https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
                     />
                     <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
                         <div className="lg:w-1/2 px-6">
@@ -24,8 +45,8 @@ export default function Contact() {
                                 ADDRESS
                             </h2>
                             <p className="mt-1">
-                                Wawa Road, <br />
-                                Tanauan Batangas, Philippines 4232
+                                97 Warren St. <br />
+                                New York, NY 10007
                             </p>
                         </div>
                         <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -33,24 +54,26 @@ export default function Contact() {
                                 EMAIL
                             </h2>
                             <a className="text-indigo-400 leading-relaxed">
-                                rlphjyson@gmail.com
+                                reedbarger@email.com
                             </a>
                             <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                                 PHONE
                             </h2>
-                            <p className="leading-relaxed">+639561432976</p>
+                            <p className="leading-relaxed">123-456-7890</p>
                         </div>
                     </div>
                 </div>
                 <form
                     netlify
                     name="contact"
+                    onSubmit={handleSubmit}
                     className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
                     <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
                         Hire Me
                     </h2>
                     <p className="leading-relaxed mb-5">
-                        Send me an email via this form.
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
+                        suscipit officia aspernatur veritatis. Asperiores, aliquid?
                     </p>
                     <div className="relative mb-4">
                         <label htmlFor="name" className="leading-7 text-sm text-gray-400">
@@ -61,6 +84,7 @@ export default function Contact() {
                             id="name"
                             name="name"
                             className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            onChange={(e) => setName(e.target.value)}
                         />
                     </div>
                     <div className="relative mb-4">
@@ -72,6 +96,7 @@ export default function Contact() {
                             id="email"
                             name="email"
                             className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            onChange={(e) => setEmail(e.target.value)}
                         />
                     </div>
                     <div className="relative mb-4">
@@ -84,6 +109,7 @@ export default function Contact() {
                             id="message"
                             name="message"
                             className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                            onChange={(e) => setMessage(e.target.value)}
                         />
                     </div>
                     <button
