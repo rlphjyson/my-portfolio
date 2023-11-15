bb1d704ccc9b802976fe3478c158b9608cc012c5
Ralph Jason Burgos <91693827+rlphjyson@users.noreply.github.com>
2023-11-15 17:26:53 +0800

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -45,21 +45,20 @@ export default function Contact() {
                                 ADDRESS
                             </h2>
                             <p className="mt-1">
-                                97 Warren St. <br />
-                                New York, NY 10007
-                            </p>
+                                <br />
+                                Tanuan, Batangas Philipines</p>
                         </div>
                         <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
                             <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                                 EMAIL
                             </h2>
                             <a className="text-indigo-400 leading-relaxed">
-                                reedbarger@email.com
+                                rlphjyson@email.com
                             </a>
                             <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                                 PHONE
                             </h2>
-                            <p className="leading-relaxed">123-456-7890</p>
+                            <p className="leading-relaxed">+639561432976</p>
                         </div>
                     </div>
                 </div>
@@ -71,10 +70,7 @@ export default function Contact() {
                     <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
                         Hire Me
                     </h2>
-                    <p className="leading-relaxed mb-5">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-                        suscipit officia aspernatur veritatis. Asperiores, aliquid?
-                    </p>
+
                     <div className="relative mb-4">
                         <label htmlFor="name" className="leading-7 text-sm text-gray-400">
                             Name
